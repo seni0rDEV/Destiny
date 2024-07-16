e5b87ec855b8895bd7000f45d9abc6991ee8c9bf
seni0rDEV <132949016+seni0rDEV@users.noreply.github.com>
2024-07-16 10:04:44 +0100

    few changes

---
+++ main.js
@@ -0,0 +1,69 @@
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+   navToggler.classList.toggle("active");
+   const nav = document.querySelector(".nav");
+   nav.classList.toggle("open");
+   if(nav.classList.contains("open")){
+     nav.style.maxHeight = nav.scrollHeight + "px";
+   }
+   else{
+     nav.removeAttribute("style");
+   }
+} 
+
+
+function setActive(element) {
+  // Remove 'active' class from all links
+  let links = document.querySelectorAll('.nav ul li a');
+  links.forEach(link => {
+      link.classList.remove('active');
+  });
+
+  // Add 'active' class to the clicked link
+  element.classList.add('active');
+}
+
+  
+
+      // TAKES YOU TO CONTACT SECTION 
+function scrollToContact() {
+  // Scroll to the contact section
+  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+
+  // Activate the corresponding navigation link
+  setActive(document.querySelector('a[href="#contact"]'));
+}
+
+function setActive(element) {
+  // Remove 'active' class from all navigation links
+  const navLinks = document.querySelectorAll('.nav ul li a');
+  navLinks.forEach(link => link.classList.remove('active'));
+
+  // Add 'active' class to the clicked navigation link
+  element.classList.add('active');
+}
+
+
+
+
+
+
+// JavaScript to close the hamburger menu when a link is clicked
+document.addEventListener('DOMContentLoaded', (event)=>{
+  function toggleMenu() {
+    document.querySelector('.nav').classList.toggle('active')
+    document.querySelector('.nav-toggler').classList.toggleMenu('active')
+  }
+  document.querySelector('.nav-toggler').addEventListener('click', toggleMenu);
+  document.querySelectorAll('.nava').forEach(navvLink => {
+    navLink.addEventListener('click', toggleMenu);
+  });
+});
+
+
+
+
+
